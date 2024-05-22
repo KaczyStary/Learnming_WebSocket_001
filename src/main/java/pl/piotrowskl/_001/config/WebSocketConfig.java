8d6e19a72f94522fbddfce4cb75271f064ebf336
Hubert <hubert.piotrowski.kmwtw@gmail.com>
2024-05-22 13:41:11 +0200

    webSocket broadcast messages

--- src/main/java/pl/piotrowskl/_001/config/WebSocketConfig.java
+++ src/main/java/pl/piotrowskl/_001/config/WebSocketConfig.java
@@ -1,2 +1,16 @@
-package pl.piotrowskl._001.config;public class WebSocketConfig {
+package pl.piotrowskl._001.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import pl.piotrowskl._001.handler.WebSocketHandler;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(new WebSocketHandler(), "/ws").setAllowedOrigins("*");
+    }
 }
--- src/main/java/pl/piotrowskl/_001/handler/WebSocketHandler.java
+++ src/main/java/pl/piotrowskl/_001/handler/WebSocketHandler.java
@@ -1,4 +1,37 @@
-package pl.piotrowskl._001.controller;
+package pl.piotrowskl._001.handler;
 
-public class WebSocketController {
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class WebSocketHandler extends TextWebSocketHandler {
+    private static Set<WebSocketSession> sessions = new HashSet<>();
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        sessions.add(session);
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        String payload = message.getPayload();
+        for (WebSocketSession s : sessions){
+            s.sendMessage(new TextMessage("User" + session.getId() + " " + payload));
+        }
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        sessions.remove(session);
+    }
+
+    @Override
+    public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
+        session.close();
+    }
+
 }
